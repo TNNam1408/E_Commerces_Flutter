dcc1f53ca8f42aa2c6f9d33d906cd5a143648de4
TNNam1408 <tnnam1408@gmail.com>
2021-09-12 19:37:58 +0700

    profile2

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -19,7 +19,7 @@ class ProfileScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text("Profile"),
+          Text("Profile Screens"),
         ],
       ),
     );
